2b74f70693d6eee0c6ad6c35e3caae0be92fd847
kamal-choudhary <itsahmedkamal@gmail.com>
2026-06-09 23:09:28 +0500

    feat(games): style games index to match Figma and add footer

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,16 @@
 module GamesHelper
+  CONDITION_LABELS = {
+    "mint"      => "Mint",
+    "excellent" => "Like New",
+    "good"      => "Good",
+    "as_is"     => "Damaged"
+  }.freeze
+
+  def game_condition_label(game)
+    CONDITION_LABELS.fetch(game.condition) { game.condition.to_s.humanize }
+  end
+
+  def game_price(game)
+    number_to_currency(game.price_cents / 100.0)
+  end
 end
